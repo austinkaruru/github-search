67887939409d61004938852830b5ddbd1a7c4c4f
Austin Karuru <karuruaustine@gmail.com>
2018-08-26 09:48:10 +0300

    added routing module ts

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+
+import { GithubComponent } from "../github/github.component";
+import { AboutComponent } from "../about/about.component";
+import { NotFoundComponent } from "../not-found/not-found.component";
+
+// Defining routes
+const routes: Routes = [
+  { path: "github", component: GithubComponent },
+  { path: "about", component: AboutComponent },
+  { path: "", redirectTo: "/goals", pathMatch: "full" },
+  { path: "**", component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: []
+})
+export class RoutingModule {}
